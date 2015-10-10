b96e8b06150c1626555de4541b544c27d3bec2d1
mattsteed <mattsteed@users.noreply.github.com>
2015-10-09 20:15:31 -0500

    Added skeleton classes for a perfect player and creature

---
+++ Creature.py
@@ -0,0 +1,11 @@
+class Creature:
+
+    def __init__(self):
+        self.chromosome = []
+        self.record = (0,0)
+
+    def mate(self, other):
+
+    def make_move(self, board):
+
+    def cross_over(chromosome_1, chromosome_2):
---
+++ PerfectPlayer.py
@@ -0,0 +1,10 @@
+class PerfectPlayer:
+
+    first_player_strat = {}
+
+    second_player_strat = {}
+    
+    def __init__(self):
+
+    def make_move(player):
+
