9adfbb9691096eae05c255360862507c4d570fde
mattsteed <mattsteed@users.noreply.github.com>
2017-05-07 00:16:20 -0500

    asfd

--- Board.py
+++ Board.py
@@ -1,53 +1,66 @@
-class TicTacToe:
+# File with definition of game board
 
-    main_dic = {0:' ', 1:'X', 2:'O'}
+import numpy as np
 
-    def __init__(self):
-        self.board = [0 for i in range(9)]
+class Board:
+  print_dict = {0:' ', 1:'X', 2:'O'}
+  def __init__(self):
+    self.board = np.zeros(9)
 
-    def add_X(self, position):
-        if self.board[position - 1] != 0:
-            return 0
-        else:
-            self.board[position - 1] = 1
-            return 1
+  def make_move(self, move, player_num):
+    if not (0 <= move and move <= 8):
+      return 0
+    if not (player_num == 1 or player_num == 2):
+      return 0
+    if self.board[move] != 0:
+      return 0
+    self.board[move] = player_num
+    return 1
 
-    def add_O(self, position):
-        if self.board[position - 1] != 0:
-            return 0
-        else:
-            self.board[position - 1] = 2
-            return 1
+  """
+  def add_O(self, position):
+    if not (0 <= position and position <= 8):
+      return 0
+    if self.board[position] != 0:
+      return 0
+    else:
+      self.board[position] = 2
+      return 1
+  """
 
-    def check_for_win(self, player_num):
-        for i in range(0,7,3):
-            if (self.board[i] == player_num and \
-                self.board[i] == self.board[i + 1]) and \
-                (self.board[i + 1] == self.board[i + 2]):
-                return 1
-        for i in range(3):
-            if (self.board[i] == player_num and \
-                self.board[i] == self.board[i + 3]) and \
-                (self.board[i + 3] == self.board[i + 6]):
-                return 1
-        if (self.board[0] == player_num and \
-            self.board[0] == self.board[4]) and \
-                (self.board[4] == self.board[8]):
-                return 1
-        if (self.board[0] == player_num and \
-            self.board[2] == self.board[4]) and \
-                (self.board[4] == self.board[6]):
-                return 1
-        return 0
+  def check_for_win(self, player_num):
+    for i in range(0,7,3):
+      if (self.board[i] == player_num and \
+          self.board[i + 1] == player_num and \
+          self.board[i + 2] == player_num):
+        return 1
+    for i in range(3):
+      if (self.board[i] == player_num and \
+          self.board[i + 3] == player_num and \
+          self.board[i + 6] == player_num):
+        return 1
+    if (self.board[0] == player_num and \
+        self.board[4] == player_num and \
+        self.board[8] == player_num):
+      return 1
+    if (self.board[2] == player_num and \
+        self.board[4] == player_num and \
+        self.board[6] == player_num):
+      return 1
+    return 0
 
-    def print_board(self):
-        print(' ' + self.main_dic[self.board[0]] + ' | ' + \
-            self.main_dic[self.board[1]] + ' | ' + self.main_dic[self.board[2]])
-        print('-----------')
-        print(' ' + self.main_dic[self.board[3]] + ' | ' + \
-            self.main_dic[self.board[4]] + ' | ' + self.main_dic[self.board[5]])
-        print('-----------')
-        print(' ' + self.main_dic[self.board[6]] + ' | ' + \
-            self.main_dic[self.board[7]] + ' | ' + self.main_dic[self.board[8]])
+  def print_board(self):
+    print(' ' + self.print_dict[self.board[0]] + ' | ' + \
+      self.print_dict[self.board[1]] + ' | ' + self.print_dict[self.board[2]])
+    print('-----------')
+    print(' ' + self.print_dict[self.board[3]] + ' | ' + \
+      self.print_dict[self.board[4]] + ' | ' + self.print_dict[self.board[5]])
+    print('-----------')
+    print(' ' + self.print_dict[self.board[6]] + ' | ' + \
+      self.print_dict[self.board[7]] + ' | ' + self.print_dict[self.board[8]])
+
+  def clear(self):
+    self.board = np.zeros(9)
+
 
 
